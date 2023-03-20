66da5069e0e7ef846a52d825a899d22c15a539fe
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-20 12:37:52 -0400

    Changed index to display Welcome to Blogs message.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res) {
+
+  try {
+    res.send("<h1>Welcome to Blogs!</h1>")
+
+  } catch (e) {
+    console.log(e);
+  }
 });
+
 
 module.exports = router;
