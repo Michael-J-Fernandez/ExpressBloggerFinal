260db6cee542af6393b3d40e6ca9adb78e38409e
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-20 12:33:27 -0400

    Created Blogs model.

---
+++ models/Blogs.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const {v4: uuidv4} = require('uuid');
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  categories: [String],
+  id: {
+    type: String,
+    default: uuidv4(),
+  }
+}, { timestamps: true, strictQuery: true });
+
+// register model to collection
+const Blog = mongoose.model("sample_blogs", blogSchema);
+
+// make our model accessible to outside files 
+module.exports = Blog;
