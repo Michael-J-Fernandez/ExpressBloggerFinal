dbcd3481be3b6af4c5a48ad2e9a0562bf198121a
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-03-30 13:06:39 -0400

    Create routes for /login and /register.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const { loginUser, registerUser } = require('../controllers/usersController')
 
-/* GET users listing. */
+
+
+const express = require('express');
+const router = express.Router();
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/login', loginUser);
+
+router.post('/register', registerUser);
 
 module.exports = router;
