5febae813fae382d5ca208f75e7f04477bf33705
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-03-30 14:20:01 -0400

    Create  getAllUser controller. Create register functionality for adding new user to db, with hashed password.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,19 +1,31 @@
-const User = require('../models/Users')
+const User = require("../models/Users");
 
-const loginUser = async (req, res) => {
+const getAllUsers = async (req, res, next) => {
+  const allUsers = await User.find({});
 
+  res.json({ users: allUsers });
+};
+
+const loginUser = async (req, res, next) => {
   res.json({
     message: "Login user!",
-    user: req.body
-  });
-}
-
-const registerUser = async (req, res) => {
-
-  res.json({
-    message: "Register user!",
-    user: req.body
+    user: req.body,
   });
 };
 
-module.exports = { loginUser, registerUser };
+const registerUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const newUser = await User.register(email, password);
+
+    res.json({
+      message: "New user registered!",
+      user: newUser,
+    });
+  } catch (error) {
+    res.json({ error: error.message })
+  }
+};
+
+module.exports = { getAllUsers, loginUser, registerUser };
