50cdd179aa862cc8d884ab8b0f7c8030aa905a11
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-03-30 14:14:45 -0400

    Add uuid to User model. Create 'register' static method.

--- models/Users.js
+++ models/Users.js
@@ -1,15 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true, strictQuery: true });
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      default: uuidv4,
+    },
+  },
+  { timestamps: true, strictQuery: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.statics.register = async function (email, password) {
+  const userExists = await this.findOne({ email });
+
+  if (userExists) {
+    throw Error("Email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await this.create({ email, password: hashedPassword });
+
+  return newUser;
+};
+
+module.exports = mongoose.model("User", userSchema);
