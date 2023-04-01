cb5708b8c5fe3e24d6b5d7037f4ec3e849a053ef
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-01 02:46:13 -0400

    Added logout route.

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,19 @@
 const express = require("express");
 
 const router = express.Router();
-const { getAllUsers, loginUser, registerUser } = require("../controllers/usersController");
+const {
+  getAllUsers,
+  registerUser,
+  loginUser,
+  logoutUser,
+} = require("../controllers/usersController");
 
 router.get("/", getAllUsers);
 
+router.post("/register", registerUser);
+
 router.post("/login", loginUser);
 
-router.post("/register", registerUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;
