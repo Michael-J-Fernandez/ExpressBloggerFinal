b9a540cf74bd8c6df6692aae80009bc79b675279
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-03-31 21:52:05 -0400

    Create 'login' static method.

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
+const { validatePassword } = require("../middleware/auth");
 
 const userSchema = mongoose.Schema(
   {
@@ -25,7 +26,7 @@ userSchema.statics.register = async function (email, password) {
   const userExists = await this.findOne({ email });
 
   if (userExists) {
-    throw Error("Email already exists");
+    throw Error("Email already exists.");
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -33,6 +34,26 @@ userSchema.statics.register = async function (email, password) {
   const newUser = await this.create({ email, password: hashedPassword });
 
   return newUser;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("Email and password must be submitted.");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("User does not exist.");
+  }
+
+  const match = validatePassword(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password.");
+  }
+
+  return user;
 };
 
 module.exports = mongoose.model("User", userSchema);
